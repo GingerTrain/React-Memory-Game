1f01d71719d18ed01e2e54e88ae3cf23d527e4f9
GingerTrain <tayloreric@rogers.com>
2018-08-22 23:39:35 -0400

    Remember to use git

--- src/App.js
+++ src/App.js
@@ -2,20 +2,212 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import angular from './img/angular.svg';
+import css from './img/css.svg';
+import html from './img/html.svg';
+import javascript from './img/javascript.svg';
+import node from './img/node.svg'
+import react from './img/react.svg';
+import vue from './img/vue.svg';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+function Card(props) {
+  return (
+    <div className='Card' onClick={props.onClick}>
+      <img className={'front-card ' + props.cardsClass} src={props.img} alt={props.alt} />
+      <img className={'back-card ' + props.cardsClass} src={react} alt={'react'} />
+    </div>
+  );
+}
+
+class Board extends Component {
+  renderCard(i) {
+    return (
+      <Card
+        cardsClass={this.props.cardsClass[i]}
+        img={this.props.img[i]}
+        // alt={JSON.stringify(this.props.img[i]).split(/[\\/]/g).pop().split('.')[0]}
+        alt={JSON.stringify(this.props.img[i]).split('/')[3].split('.')[0]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {/* 4 x 3 board */}
+        <div className='Board-row'>
+          {this.renderCard(0)}
+          {this.renderCard(1)}
+          {this.renderCard(2)}
+        </div>
+        <div className='Board-row'>
+          {this.renderCard(3)}
+          {this.renderCard(4)}
+          {this.renderCard(5)}
+        </div>
+        <div className='Board-row'>
+          {this.renderCard(6)}
+          {this.renderCard(7)}
+          {this.renderCard(8)}
+        </div>
+        <div className='Board-row'>
+          {this.renderCard(9)}
+          {this.renderCard(10)}
+          {this.renderCard(11)}
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: shuffleCards(),
+      cardsClass: Array(12).fill(''),
+      hasFlippedCard: false,
+      firstCard: {img: '', pos: '',},
+      secondCard: {img: '', pos: '',},
+    };
+  }
+
+  handleClick(i) {
+    var cards = this.state.cards.slice();
+    var cardsClass = this.state.cardsClass.slice();
+    var hasFlippedCard = this.state.hasFlippedCard;
+    var firstCard = this.state.firstCard;
+    var secondCard = this.state.secondCard;
+
+    // Prevent user from clicking an already flipped card
+    if(cardsClass[i] === 'flip') {
+      return;
+    }
+
+    if(calculateWinner(cardsClass)) {
+      return;
+    }
+
+    // If a card is already flipped
+    if(hasFlippedCard) {
+      secondCard.img = cards[i];
+      secondCard.pos = i;
+
+      if(isMatch(firstCard, secondCard)) {
+        cards[i] = cards[i];
+        cardsClass[i] = 'flip';
+      } else {
+        cardsClass = unflipCards(firstCard, secondCard, cardsClass);
+      }
+    } else {
+      firstCard.img = cards[i];
+      firstCard.pos = i;
+
+      cards[i] = cards[i];
+      cardsClass[i] = 'flip';
+    }
+
+    // Update state accordingly
+    this.setState({
+      cards: cards,
+      cardsClass: cardsClass,
+      hasFlippedCard: !this.state.hasFlippedCard,
+      firstCard: firstCard,
+      secondCard: secondCard,
+    });
+  }
+
+  render() {
+    return (
+      <div className='Game'>
+        <div className='Game-Board'>
+          <Board
+            img={this.state.cards}
+            cardsClass={this.state.cardsClass}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// Shuffles order of cards each new game
+function shuffleCards() {
+  let cards = [angular, angular, css, css, html, html, javascript, javascript, node, node, vue, vue];
+  // Fisher-Yates Shuffle adapted from: https://bost.ocks.org/mike/shuffle/
+  let counter = cards.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = cards[counter];
+        cards[counter] = cards[index];
+        cards[index] = temp;
+    }
+
+    return cards;
+}
+
+// Checkes if the two flipped cards match
+function isMatch(firstCard, secondCard) {
+  return (firstCard.img == secondCard.img ? true : false);
+}
+
+// Unflips the two flipped cards
+function unflipCards(firstCard, secondCard, cardsClass) {
+  cardsClass[firstCard.pos] = '';
+  cardsClass[secondCard.pos] = '';
+
+  return cardsClass;
+}
+
+// Checks if user won
+function calculateWinner(cardsClass) {
+  console.log(cardsClass.every((val, arr) => val === arr[0]));
+  return (cardsClass.every((val, arr) => val === arr[0]));
+}
+
+export default Game;
+
+{/* 3 x 4 board */}
+        {/* <div className='Board-row'>
+          {this.renderCard(0)}
+          {this.renderCard(1)}
+          {this.renderCard(2)}
+          {this.renderCard(3)}
+        </div>
+        <div className='Board-row'>
+          {this.renderCard(4)}
+          {this.renderCard(5)}
+          {this.renderCard(6)}
+          {this.renderCard(7)}
+        </div>
+        <div className='Board-row'>
+          {this.renderCard(8)}
+          {this.renderCard(9)}
+          {this.renderCard(10)}
+          {this.renderCard(11)} */}
